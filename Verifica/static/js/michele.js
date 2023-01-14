f60e2a746c789138cd0855819ad6d68c508a1c14
MicheleB1tetto <116425760+MicheleB1tetto@users.noreply.github.com>
2023-01-14 09:21:51 +0100

    Add files via upload

---
+++ Verifica/static/js/michele.js
@@ -0,0 +1,25 @@
+		function setup() {
+			createCanvas(600,600);
+			background(200);
+        }
+        function draw() {
+        	nostroke()
+        	fill(255,255,255)
+            circle(300,300,555)
+        	fill(0,0,0)
+        	circle(300,300,500)
+        	fill(255,255,255)
+            circle(300,300,445)
+            fill(0,0,0)
+            circle(300,300,390)
+            fill(255,255,255)
+          circle(300,300,335)
+          fill(0,0,0)
+          circle(300,300,280)
+          fill(255,255,255)
+          circle(300,300,225)
+          fill(0,0,0)
+          circle(300,300,170)
+          fill(210,0,0)
+          circle(300,300,90)
+        }
